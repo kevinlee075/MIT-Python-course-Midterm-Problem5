26c848512819f14ccf27846887d0328c378e0e4b
kevinlee075 <kevinlee075@gmail.com>
2021-07-04 16:40:17 +0800

    Update MIT-Python-course-Midterm-Problem5.py

--- MIT-Python-course-Midterm-Problem5.py
+++ MIT-Python-course-Midterm-Problem5.py
@@ -5,6 +5,13 @@ Created on Sun Jul  4 16:29:13 2021
 @author: Lenovo
 """
 
+#Write a Python function that returns a list of keys in aDict that map to integer\
+#values that are unique (i.e. values appear exactly once in aDict).
+#The list of keys you return should be sorted in increasing order.
+#(If aDict does not contain any unique values, you should return an empty list.)
+
+#This function takes in a dictionary and returns a list.
+
 def uniqueValues(aDict):
     '''
     aDict: a dictionary
